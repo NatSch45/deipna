893f1238b085c3b3e71be61527c796331330b5d3
Nathan Schneider <nathan.schneider4505@gmail.com>
2026-02-22 13:43:13 +0100

    [back] Fix env environment variables issue during deployment

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,15 +1,19 @@
+const path = require('path')
+
+const buildDir = path.join(__dirname, 'build')
+const envFile = path.join(buildDir, '.env')
+
 module.exports = {
   apps: [
     {
       name: 'deipna-backend',
-      script: './build/bin/server.js',
+      script: path.join(buildDir, 'bin/server.js'),
+      cwd: buildDir,
+      node_args: `--env-file=${envFile}`,
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
-      env_production: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 }
